3f432c0cbb6d583e505b545451f9a38f218d4527
Aryendel <nuildtk@gmail.com>
2024-10-21 14:31:34 +0200

    F

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -19,4 +19,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long2Type));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-} return(0); 
+
+		return(0);
+}
