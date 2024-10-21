ff9ccdedb8986884f99d9a3b1264bbbab535dc3b
Aryendel <nuildtk@gmail.com>
2024-10-21 14:18:33 +0200

    F

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-char charType;
-int intType;
-long int longType;
-long long int long2Type;
-float floatType;
+	char charType;
+	int intType;
+	long int longType;
+	long long int long2Type;
+	float floatType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
