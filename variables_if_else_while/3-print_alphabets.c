3240ccd3676ea21aa5eabd873dea6ac9769df30e
Aryendel <nuildtk@gmail.com>
2024-10-22 15:05:49 +0200

    F

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 97;
+int i2 = 65;
+do {
+putchar(i);
+i++;
+} while (i <= 122);
+do {
+putchar(i2);
+i++;
+} while (i2 <= 90);
+putchar('\n');
+return (0);
+}
