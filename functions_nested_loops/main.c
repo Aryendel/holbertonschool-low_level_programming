6bf55f3d1d22b9fbc99d050654b5ee28d8a8b4e2
Aryendel <nuildtk@gmail.com>
2024-10-23 19:09:58 +0200

    F

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,17 +1,12 @@
 #include "main.h"
-/**
- * print_alphabet - prints the alphabet in lowercase
- *
- * Return: Nothing.
- */
-void print_alphabet(void)
-{
-	int i;
 
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-_putchar('\n');
-return;
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
