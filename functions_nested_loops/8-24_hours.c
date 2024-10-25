1b951fbba2beed10915782eaf173814a24fcc925
Aryendel <nuildtk@gmail.com>
2024-10-25 16:08:28 +0200

    F

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,22 +1,24 @@
 #include "main.h"
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer.
- */
-void jack_bauer(void)
+int main(void)
 {
-	int min1, min2, total;
+	int n, hour1, hour2, min1, min2, total;
 
 	for (total = 0; total <= 1440; total++)
-	{
-/** Code pour les MINUTES */
+	{   
+		for (hour1 = 48; hour1 <= 50; hour1++)
 
-		for (min1 = 48; min1 <= 53; min1++)
+			for (hour2 = 48; hour2 <= 57; hour2++)
 
-			for (min2 = 48; min2 <= 57; min2++)
-			{
-				putchar(':');
-				putchar(min1);
-				putchar(min2);
-				putchar('\n');
-			}
+				for (min1 = 48; min1 <= 53; min1++)
+
+					for (min2 = 48; min2 <= 57; min2++)
+					{
+						putchar(hour1);
+						putchar(hour2);
+						putchar(':');
+						putchar(min1);
+						putchar(min2);
+						putchar('\n');
+					}
 	}
+}
