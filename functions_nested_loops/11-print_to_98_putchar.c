b4a2ffcf5c59550e363e33ceaeb1cd18dd52d643
Aryendel <nuildtk@gmail.com>
2024-10-26 12:12:30 +0200

    F

--- functions_nested_loops/11-print_to_98_putchar.c
+++ functions_nested_loops/11-print_to_98_putchar.c
@@ -14,31 +14,44 @@ void print_to_98(int n)
 			{
 				if (n < -9 || n > 9)
 				{
-					_putchar(n + '0');
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					if (n < 0)
+					{
+						_putchar((-n / 10) + '0');
+						_putchar((-n % 10) + '0');
+					}
 				}
 				else
 				{
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
+					_putchar(n + '0');
 				}
 			}
 			else
 			{
 				if (n < -9 || n > 9)
 				{
-					_putchar(n + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
 					_putchar((n / 10) + '0');
 					_putchar((n % 10) + '0');
 					_putchar(',');
 					_putchar(' ');
+					if (n < 0)
+					{
+						_putchar((-n / 10) + '0');
+						_putchar((-n % 10) + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
 				}
-			}	
+				else
+				{
+					_putchar(n + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
+		_putchar('\n');
 	}
 	else
 	{
@@ -51,32 +64,45 @@ void print_to_98(int n)
 
 					if (n < -9 || n > 9)
 					{
-						_putchar(n + '0');
+						_putchar((n / 10) + '0');
+						_putchar((n % 10) + '0');
+						if (n < 0)
+						{
+							_putchar((-n / 10) + '0');
+							_putchar((-n % 10) + '0');
+						}
 					}
 					else
 					{
-						_putchar((n / 10) + '0');
-						_putchar((n % 10) + '0');
+						_putchar(n + '0');
 					}
 				}
 				else
 				{
 					if (n < -9 || n > 9)
 					{
-						_putchar(n + '0');
+						_putchar((n / 10) + '0');
+						_putchar((n % 10) + '0');
 						_putchar(',');
 						_putchar(' ');
+						if (n < 0)
+						{
+							_putchar((-n / 10) + '0');
+							_putchar((-n % 10) + '0');
+							_putchar(',');
+							_putchar(' ');
+						}
 					}
 					else
 					{
-						_putchar((n / 10) + '0');
-						_putchar((n % 10) + '0');
+						_putchar(n + '0');
 						_putchar(',');
 						_putchar(' ');
 					}
 				}
 
 			}
+			_putchar('\n');
 		}
 	}
 }
