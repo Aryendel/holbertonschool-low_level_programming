ad0796936fc71e7bb8869af75eeecbc5a1df3ea6
Aryendel <nuildtk@gmail.com>
2024-10-28 09:46:21 +0100

    F

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _isupper - checks for uppeercase character
+ * @c: character
+ * Return: 1 if c is uppercase; 0 otherwise
+ */
+int _isupper(int c);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+#endif
