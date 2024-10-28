f5d889d56f6e319a4b065e66d79e5a3e81a865f3
Aryendel <nuildtk@gmail.com>
2024-10-28 21:00:41 +0100

    F

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,22 +7,24 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		printf("%d ", i);
-
 		if (i % 3 == 0)
 		{
 			printf("Fizz ");
+			continue;
 		}
 		if (i % 5 == 0)
 		{
 			printf("Buzz ");
+			continue;
 		}
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
+			continue;
 		}
+		printf("%d ", i);
 
 	}
 	return (0);
