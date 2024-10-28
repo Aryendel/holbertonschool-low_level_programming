829703535ebad6bc9df10dc1750678c85b6c6509
Aryendel <nuildtk@gmail.com>
2024-10-28 10:44:45 +0100

    F

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@ void print_numbers(void)
 
 	int numbers;
 
-	for (numbers = 48; numbers <= 57; numbers++)
+	for (numbers = 0; numbers <= 9; numbers++)
 	{
-		_putchar(numbers);
+		_putchar(numbers + '0');
 	}
 	_putchar('\n');
 }
