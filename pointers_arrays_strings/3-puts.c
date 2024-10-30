cffcaabb867ccfdb822686b6890950136424fa6d
Aryendel <nuildtk@gmail.com>
2024-10-30 10:13:46 +0100

    F

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** _puts - prints a string to stdout
+ * @str: string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	while(str[i]) 
+	{
+		if(_putchar(str[i]) == '\0')
+		{
+			return;
+		}
+		i++;
+	}
+	if(_putchar('\n') == '\0') 
+	{
+		return;
+	}
+	return;
+}
